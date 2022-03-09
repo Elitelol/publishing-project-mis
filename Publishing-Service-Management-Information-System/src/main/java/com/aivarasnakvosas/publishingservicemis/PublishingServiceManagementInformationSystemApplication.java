04afe198417cf7c80d4f13201a46a7bf88470a14
Elitelol <58183375+Elitelol@users.noreply.github.com>
2022-03-09 22:06:50 +0200

    Init Spring Boot app

---
+++ Publishing-Service-Management-Information-System/src/main/java/com/aivarasnakvosas/publishingservicemis/PublishingServiceManagementInformationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.aivarasnakvosas.publishingservicemis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublishingServiceManagementInformationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PublishingServiceManagementInformationSystemApplication.class, args);
+    }
+
+}
