a6645c28348dea962dcc8174b53c9a3dd247f2c3
Elitelol <58183375+Elitelol@users.noreply.github.com>
2022-04-23 00:17:38 +0300

    fix psw

--- Publishing-Service-Management-Information-System/src/main/java/com/aivarasnakvosas/publishingservicemis/mappers/UserDTOMapper.java
+++ Publishing-Service-Management-Information-System/src/main/java/com/aivarasnakvosas/publishingservicemis/mappers/UserDTOMapper.java
@@ -4,6 +4,7 @@ package com.aivarasnakvosas.publishingservicemis.mappers;
 import com.aivarasnakvosas.publishingservicemis.entity.User;
 import com.aivarasnakvosas.publishingservicemis.dtos.UserDTO;
 import com.aivarasnakvosas.publishingservicemis.entity.enums.Role;
+import com.aivarasnakvosas.publishingservicemis.exceptions.BusinessErrorException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -20,7 +21,10 @@ public class UserDTOMapper {
     public void mapToUser(User user, UserDTO userDTO) {
         user.setUsername(userDTO.getUsername());
         String password = userDTO.getPassword();
-        if (password != null && password.isEmpty()) {
+        if (user.getId() == null && (password == null || password.isBlank())) {
+            throw new BusinessErrorException("Password can't be empty.");
+        }
+        if (password != null && !password.isBlank()) {
             user.setPassword(passwordEncoder.encode(password));
         }
         user.setFirstName(userDTO.getFirstName());
