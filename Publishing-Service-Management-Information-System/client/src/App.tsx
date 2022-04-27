3dc8840136500af2b840b32ff25d7190db438c86
Elitelol <58183375+Elitelol@users.noreply.github.com>
2022-04-27 22:41:42 +0300

    Add Sign up modal

--- Publishing-Service-Management-Information-System/client/src/App.tsx
+++ Publishing-Service-Management-Information-System/client/src/App.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 import './App.css';
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
+import Nav from "./components/Nav";
+import Hero from "./components/Hero";
 
 function App() {
 
   return (
-    <div className="App">
-      Hello world
-        <Login />
-        <SignUp/>
+    <div>
+      <Nav/>
+        <Hero/>
     </div>
   );
 }
